a7d736f6dba2aac60c35033c68eedaf050445469
Marina Miranovich <marina.mironovich@gmail.com>
2017-09-11 20:08:05 -0700

    add deployment script, add isomorphic fetch polyfill

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,3 +1,8 @@
+import esPromise from 'es6-promise';
+esPromise.polyfill();
+
+import 'isomorphic-fetch';
+
 export const search = (q) => {
   return fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${q}&maxResults=9&key=AIzaSyCvIQlSdzdlpOzowcdpP3hE4cYzKFYNnxg`, {
     method: 'GET'

81d071f2137c2428426d23c36f121ec45fdebccb
Marina Miranovich <marina.mironovich@gmail.com>
2017-09-14 14:52:23 -0700

    add deployment for the static assets

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,7 +1,6 @@
 import esPromise from 'es6-promise';
-esPromise.polyfill();
-
 import 'isomorphic-fetch';
+esPromise.polyfill();
 
 export const search = (q) => {
   return fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${q}&maxResults=9&key=AIzaSyCvIQlSdzdlpOzowcdpP3hE4cYzKFYNnxg`, {
